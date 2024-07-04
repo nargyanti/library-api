9772c128bb207acfedc0c5670e0baf478135e947
nargyanti <nargyanti@gmail.com>
2024-07-04 23:35:54 +0700

    Refresh migration

--- prisma/migrations/20240704163526_init/migration.sql
+++ prisma/migrations/20240704163526_init/migration.sql
@@ -21,11 +21,11 @@ CREATE TABLE "Book" (
 -- CreateTable
 CREATE TABLE "BookLoan" (
     "id" SERIAL NOT NULL,
-    "member_id" INTEGER NOT NULL,
-    "book_id" INTEGER NOT NULL,
-    "borrow_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "return_at" TIMESTAMP(3),
-    "is_returned" BOOLEAN NOT NULL DEFAULT false,
+    "member" INTEGER NOT NULL,
+    "book" INTEGER NOT NULL,
+    "borrowAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "returnAt" TIMESTAMP(3),
+    "isReturned" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "BookLoan_pkey" PRIMARY KEY ("id")
 );
@@ -33,9 +33,9 @@ CREATE TABLE "BookLoan" (
 -- CreateTable
 CREATE TABLE "Penalty" (
     "id" SERIAL NOT NULL,
-    "member_id" INTEGER NOT NULL,
-    "start_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "end_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "member" INTEGER NOT NULL,
+    "startAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Penalty_pkey" PRIMARY KEY ("id")
 );
@@ -47,10 +47,10 @@ CREATE UNIQUE INDEX "Member_code_key" ON "Member"("code");
 CREATE UNIQUE INDEX "Book_code_key" ON "Book"("code");
 
 -- AddForeignKey
-ALTER TABLE "BookLoan" ADD CONSTRAINT "BookLoan_member_id_fkey" FOREIGN KEY ("member_id") REFERENCES "Member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "BookLoan" ADD CONSTRAINT "BookLoan_member_fkey" FOREIGN KEY ("member") REFERENCES "Member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "BookLoan" ADD CONSTRAINT "BookLoan_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "BookLoan" ADD CONSTRAINT "BookLoan_book_fkey" FOREIGN KEY ("book") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Penalty" ADD CONSTRAINT "Penalty_member_id_fkey" FOREIGN KEY ("member_id") REFERENCES "Member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Penalty" ADD CONSTRAINT "Penalty_member_fkey" FOREIGN KEY ("member") REFERENCES "Member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
